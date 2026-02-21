b3eabc51766f85e59b555241fe990ba1cc1c7f6c
Jure <juremiskov@gmail.com>
2026-02-21 03:03:55 +0100

    update serious

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    compressHTML: false
+    compressHTML: false,
+    site: "https://project-planet-astro.netlify.app/"
 });
